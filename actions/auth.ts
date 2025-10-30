286c71a71cbb8d91fcb11686c426400464e807e4
edem8 <eddieblack159@gmail.com>
2025-10-30 14:42:22 +0000

    feat: sync auth w/ supabse

---
+++ actions/auth.ts
@@ -0,0 +1,21 @@
+"use server";
+
+import { createClient } from "@/supabase/server";
+
+export const authenticate = async (email: string, password: string) => {
+  try {
+    const supabase = await createClient();
+
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+
+    if (error) {
+      throw error;
+    }
+  } catch (error) {
+    console.log("AUTENTICATION ERRROR ", error);
+    throw error;
+  }
+};
